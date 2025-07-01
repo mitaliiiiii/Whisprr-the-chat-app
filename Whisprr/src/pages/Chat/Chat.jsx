b8c688cb815e8ff23db6957439e67f9ba85ac8e2
mitali <mitali7mita7mitali@gmail.com>
2025-07-01 10:36:24 +0530

    improvemennts done

--- Whisprr/src/pages/Chat/Chat.jsx
+++ Whisprr/src/pages/Chat/Chat.jsx
@@ -12,15 +12,32 @@ const Chat = () => {
     <div className="outsidebox">
       <div className="outsideborder">
                  <img src={assets.starsImg} alt="" className='stars' /> 
-         <div className="1"><img src={assets.cloud} alt="" className='cloud cloud1' /> </div>
-         <div className="1"><img src={assets.cloud} alt="" className='cloud cloud2' /> </div>
+         <div className="1"><img src={assets.cloud} alt="" className='cloud clouddd1' /> </div>
+         <div className="1"><img src={assets.cloud} alt="" className='cloud clouddd2' /> </div>
         <div className="leftside">
           <span className='fontstylee cap'>MY CHATS</span>
           <div className="leftbox">
+            <div className="chatsearch">
+              
+              <input type="text" className='searchin fontstyle' name="" id="" placeholder='Search Chats'/>
+              <img src={assets.search_icon}  alt="" /></div>
+              <div className='chatbox'>
             <div className="chat"></div>
             <div className="chat"></div>
             <div className="chat"></div>
             <div className="chat"></div>
+            <div className="chat"></div>
+            <div className="chat"></div>
+            <div className="chat"></div>
+            <div className="chat"></div>
+            <div className="chat"></div>
+            <div className="chat"></div>
+            <div className="chat"></div>
+            <div className="chat"></div>
+            <div className="chat"></div>
+            <div className="chat"></div>
+         
+            <div className="chat"></div></div>
           </div>
         </div>
         
